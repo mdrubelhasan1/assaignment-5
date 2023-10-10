f3b6640009aa04ae722255ddb164fe92f7ed9dd6
mdrubelhasan1 <mdrubelhasan360@gmail.comm>
2023-10-10 09:46:12 +0600

    parallelogram and rhombus calculate

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -35,4 +35,56 @@ document.getElementById('btn-rectangle').addEventListener('click',function(){
 });
 
 //Parallelogram area calculate:
+document.getElementById('area-parallelogram').addEventListener('click',function(){
+    const base = getInputFieldValueById('parallelogram-input1');
+    const height = getInputFieldValueById('parallelogram-input2');
+    const area = base * height;
+    setTextElementValueById('parallelogram-area',area);
+
+     // Validate check: number and negative number:
+     if(isNaN(base) || isNaN(height)){
+        alert('Please enter a number');
+    }
+    if(base < 0 || height < 0){
+        alert('Please enter positive number');
+        return;
+    }
+});
+
+// Rhombus area calculate:
+
+document.getElementById('btn-rhombus').addEventListener('click',function(){
+    const d1 = getInputFieldValueById('input-d1');
+    const d2 = getInputFieldValueById('input-d2');
+    const area = 0.5 * d1 * d2;
+    setTextElementValueById('rhombus-area', area);
+
+     // Validate check: number and negative number:
+     if(isNaN(d1) || isNaN(d2)){
+        alert('Please enter a number');
+    }
+    if(d1 < 0 || d2 < 0){
+        alert('Please enter positive number');
+        return;
+    }
+});
+
+// Pentagon area calculate:
+document.getElementById('btn-pentagone').addEventListener('click', function(){
+    const valueP = getInputFieldValueById('input-p');
+    const valueB = getInputFieldValueById('input-b');
+    const area = 0.5 * valueP * valueB;
+    setTextElementValueById('pentagon-area', area);
+
+     // Validate check: number and negative number:
+     if(isNaN(valueP) || isNaN(valueB)){
+        alert('Please enter a number');
+    }
+    if(valueP < 0 || valueB < 0){
+        alert('Please enter positive number');
+        return;
+    }
+});
+
+// Ellipse area calculate:
 
